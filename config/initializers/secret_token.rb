76d6fb03924e871eba96e5af6de04781bd3ba535
Becca Sheldon <rebecca.e.sheldon@gmail.com>
2013-10-24 16:43:23 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beccapp::Application.config.secret_key_base = 'ba90cfd0335c4eb6cf5fa769f3af6f663e2f4fc20165f00623f6e29832ce5f4754c2599188524c4395db7263efa7a1eaef08dbd836778359b1d51342d45d08ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Beccapp::Application.config.session_store :cookie_store, key: '_beccapp_session'
